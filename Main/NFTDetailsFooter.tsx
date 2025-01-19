064a168691abe6793bf21af40fc484fcebfd0cd5
cprhero7744 <137731710+cprhero7744@users.noreply.github.com>
2025-01-18 19:47:12 -0500

    Create NFTDetailsFooter.tsx

---
+++ Main/NFTDetailsFooter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NFTDetailsFooter.css";
+
+const NFTDetailsFooter: React.FC = () => {
+  return (
+    <footer className="nft-details-footer">
+      <p>&copy; 2025 LIGHTDAO. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default NFTDetailsFooter;
