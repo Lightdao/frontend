fcde9729f0d237b918769ec9a95877158525260f
cprhero7744 <137731710+cprhero7744@users.noreply.github.com>
2025-01-18 19:52:43 -0500

    Create DashboardSocialImpact.tsx

---
+++ Main/DashboardSocialImpact.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./DashboardSocialImpact.css";
+
+const DashboardSocialImpact: React.FC = () => {
+  return (
+    <section className="social-impact">
+      <h2>Social Impact</h2>
+      <div className="chart">
+        <p>[Insert Pie Chart Here]</p>
+      </div>
+    </section>
+  );
+};
+
+export default DashboardSocialImpact;
