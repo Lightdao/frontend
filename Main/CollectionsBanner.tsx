a73efc394d2981a437528c1d236e73fe70d4a1ba
cprhero7744 <137731710+cprhero7744@users.noreply.github.com>
2025-01-18 19:23:17 -0500

    Update CollectionsBanner.tsx

--- Main/CollectionsBanner.tsx
+++ Main/CollectionsBanner.tsx
@@ -1,1 +1,24 @@
+import React from "react";
+import "./CollectionsBanner.css";
+
+const CollectionsBanner: React.FC = () => {
+  return (
+    <div className="collections-banner">
+      <img src="/path/to/banner.jpg" alt="Banner" className="collections-banner-image" />
+      <h1>8SIAN Main Collection</h1>
+      <div className="collections-stats">
+        <span>8K Items</span>
+        <span>2.5K Owners</span>
+        <span>3.2K Volume Traded</span>
+        <span>5.4K Likes</span>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet varius quam, nec
+        convallis arcu.
+      </p>
+    </div>
+  );
+};
+
+export default CollectionsBanner;
 
