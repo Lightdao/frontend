d5aeef8e07dcc61893b05c3e30c1a81fb9838c65
cprhero7744 <137731710+cprhero7744@users.noreply.github.com>
2025-01-18 19:10:01 -0500

    Create NFTCard.tsx

---
+++ Main/NFTCard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./NFTCard.css";
+
+type NFTCardProps = {
+  title: string;
+  price: string;
+  image: string;
+};
+
+const NFTCard: React.FC<NFTCardProps> = ({ title, price, image }) => {
+  return (
+    <div className="nft-card">
+      <img className="nft-image" src={image} alt={title} />
+      <div className="nft-info">
+        <h3>{title}</h3>
+        <p>{price} SOL</p>
+      </div>
+    </div>
+  );
+};
+
+export default NFTCard;
