cdb445bf7a305399fd7997b853e50388c121a1ef
cprhero7744 <137731710+cprhero7744@users.noreply.github.com>
2025-01-18 19:52:09 -0500

    Create DashboardSalesHistory.tsx

---
+++ Main/DashboardSalesHistory.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./DashboardSalesHistory.css";
+
+const DashboardSalesHistory: React.FC = () => {
+  return (
+    <section className="sales-history">
+      <h2>Sales History</h2>
+      <div className="chart">
+        <p>[Insert Chart Here]</p>
+      </div>
+    </section>
+  );
+};
+
+export default DashboardSalesHistory;
