88b2ff8b386cf312b45025dd0dce3ea8c3a457a2
cprhero7744 <137731710+cprhero7744@users.noreply.github.com>
2025-03-22 06:24:23 -0400

    Update NewListingsStep2.tsx

--- Main/NewListingsStep2.tsx
+++ Main/NewListingsStep2.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './NewListingsStep2.css';
 
-const UploadItem = () => {
+const UploadItem: React.FC = () => {
   const [selected, setSelected] = useState<'single' | 'collection' | null>(null);
 
   return (
